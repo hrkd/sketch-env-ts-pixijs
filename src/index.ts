d780232d52a091d50a94fb47aad728a22f7f8e74
hrkd <hiroki@hrkd.net>
2021-06-04 20:40:14 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import './styles/index.scss';
-import { customThreejsPipelineModule } from './pipelineModules/customThreejsPipelineModule';
+import { initScenePipelineModule } from './pipelineModules/initScenePipelineModule';
 
 const onxrloaded = () => {
   window.XR8.addCameraPipelineModules([
     // Add camera pipeline modules.
     window.XR8.GlTextureRenderer.pipelineModule(), // Draws the camera feed.
+    window.XR8.Threejs.pipelineModule(), // Creates a ThreeJS AR Scene.
     window.XR8.XrController.pipelineModule(), // Enables SLAM tracking.
     window.XRExtras.AlmostThere.pipelineModule(), // Detects unsupported browsers and gives hints.
     window.XRExtras.FullWindowCanvas.pipelineModule(), // Modifies the canvas to fill the window.
     window.XRExtras.Loading.pipelineModule(), // Manages the loading screen on startup.
     window.XRExtras.RuntimeError.pipelineModule(), // Shows an error image on runtime error.
     // Other custom pipeline modules.
-    customThreejsPipelineModule(),
+    initScenePipelineModule(),
   ]);
 
   // Open the camera and start running the camera run loop.
